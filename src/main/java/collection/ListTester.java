a3d3f13798e181e2f4367caca5f148d12a77e0e6
陈平平 <chenpingping@xiao100.com>
2021-09-22 17:12:39 +0800

    feat:Arrays.asList方法使用陷阱

---
+++ src/main/java/collection/ListTester.java
@@ -0,0 +1,31 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @Author chenpp【chenpingping1@xiao100.com】
+ * @Date  2020年12月24日
+ */
+public class ListTester {
+    
+    public static void main(String[] args) {
+//        rightUse();
+        wrongUse();
+    }
+    
+    private static void wrongUse() {
+        List<String> list = Arrays.asList("Java8", "Java9", "Java10");
+        // 会报错，为啥？ Arrays.asList返回的ArrayList是Arrays的静态内部类，没有add方法
+        list.add("Java 11");
+        System.out.println(list);
+    }
+    
+    private static void rightUse() {
+        List<String> list = new ArrayList<>(Arrays.asList("Java8", "Java9", "Java10"));
+        list.add("Java 11");
+        System.out.println(list);
+    }
+}
